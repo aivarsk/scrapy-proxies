be8164ac5e9faa53dfaf7a4e24eebc91413be1da
Aivars Kalvans <aivars.kalvans@gmail.com>
2014-03-19 16:14:20 +0200

    Don't overwrite proxy with a new random one

--- randomproxy.py
+++ randomproxy.py
@@ -26,11 +26,11 @@ from scrapy import log
 class RandomProxy(object):
     def __init__(self, settings):
         self.proxy_list = settings.get('PROXY_LIST')
-        f = open(self.proxy_list)
+        fin = open(self.proxy_list)
 
         self.proxies = {}
-        for l in f.readlines():
-            parts = re.match('(\w+://)(\w+:\w+@)?(.+)', l)
+        for line in fin.readlines():
+            parts = re.match('(\w+://)(\w+:\w+@)?(.+)', line)
 
             # Cut trailing @
             if parts[1]:
@@ -38,22 +38,30 @@ class RandomProxy(object):
 
             self.proxies[parts[0] + parts[2]] = parts[1]
 
-        f.close()
+        fin.close()
 
     @classmethod
     def from_crawler(cls, crawler):
         return cls(crawler.settings)
 
     def process_request(self, request, spider):
+        # Don't overwrite with a random one (server-side state for IP)
+        if 'proxy' in request.meta:
+            return
+
         proxy_address = random.choice(self.proxies.keys())
         proxy_user_pass = self.proxies[proxy_address]
 
         request.meta['proxy'] = proxy_address
         if proxy_user_pass:
-            request.headers['Proxy-Authorization'] = 'Basic ' + base64.encodestring(proxy_user_pass)
+            basic_auth = 'Basic ' + base64.encodestring(proxy_user_pass)
+            request.headers['Proxy-Authorization'] = basic_auth
 
     def process_exception(self, request, exception, spider):
         proxy = request.meta['proxy']
-        log.msg('Removing failed proxy <%s>, %d proxies left' % (proxy, len(self.proxies)))
-        try: del self.proxies[proxy]
-        except ValueError: pass
+        log.msg('Removing failed proxy <%s>, %d proxies left' % (
+                    proxy, len(self.proxies)))
+        try:
+            del self.proxies[proxy]
+        except ValueError:
+            pass
