5a5b45310ddb103169848d3aacf287ba6be6cbd2
Aurélien <aurelien.schiltz@epitech.eu>
2016-10-17 16:07:54 +0200

    Added 3 modes and fixed the retry

--- scrapy_proxies/randomproxy.py
+++ scrapy_proxies/randomproxy.py
@@ -25,30 +25,51 @@ import logging
 
 log = logging.getLogger('scrapy.proxies')
 
+
+class Mode:
+    RANDOMIZE_PROXY_EVERY_REQUESTS, RANDOMIZE_PROXY_ONCE, SET_CUSTOM_PROXY = range(3)
+
+
 class RandomProxy(object):
     def __init__(self, settings):
+        self.mode = settings.get('PROXY_MODE')
         self.proxy_list = settings.get('PROXY_LIST')
-
+        self.chosen_proxy = ''
         if self.proxy_list is None:
             raise KeyError('PROXY_LIST setting is missing')
 
-        fin = open(self.proxy_list)
+        if self.mode == Mode.RANDOMIZE_PROXY_EVERY_REQUESTS or self.mode == Mode.RANDOMIZE_PROXY_ONCE:
+            fin = open(self.proxy_list)
+            self.proxies = {}
+            for line in fin.readlines():
+                parts = re.match('(\w+://)(\w+:\w+@)?(.+)', line.strip())
+                if not parts:
+                    continue
 
-        self.proxies = {}
-        for line in fin.readlines():
-            parts = re.match('(\w+://)(\w+:\w+@)?(.+)', line.strip())
+                # Cut trailing @
+                if parts.group(2):
+                    user_pass = parts.group(2)[:-1]
+                else:
+                    user_pass = ''
+
+                self.proxies[parts.group(1) + parts.group(3)] = user_pass
+            fin.close()
+            if self.mode == Mode.RANDOMIZE_PROXY_ONCE:
+                self.chosen_proxy = random.choice(list(self.proxies.keys()))
+        elif self.mode == Mode.SET_CUSTOM_PROXY:
+            custom_proxy = settings.get('CUSTOM_PROXY')
+            self.proxies = {}
+            parts = re.match('(\w+://)(\w+:\w+@)?(.+)', custom_proxy.strip())
             if not parts:
-                continue
+                raise ValueError('CUSTOM_PROXY is not well formatted')
 
-            # Cut trailing @
             if parts.group(2):
                 user_pass = parts.group(2)[:-1]
             else:
                 user_pass = ''
 
             self.proxies[parts.group(1) + parts.group(3)] = user_pass
-
-        fin.close()
+            self.chosen_proxy = parts.group(1) + parts.group(3)
 
     @classmethod
     def from_crawler(cls, crawler):
@@ -57,30 +78,39 @@ class RandomProxy(object):
     def process_request(self, request, spider):
         # Don't overwrite with a random one (server-side state for IP)
         if 'proxy' in request.meta:
-            return
-
+            if request.meta["exception"] is False:
+                return
+        request.meta["exception"] = False
         if len(self.proxies) == 0:
             raise ValueError('All proxies are unusable, cannot proceed')
 
-        proxy_address = random.choice(list(self.proxies.keys()))
+        if self.mode == Mode.RANDOMIZE_PROXY_EVERY_REQUESTS:
+            proxy_address = random.choice(list(self.proxies.keys()))
+        else:
+            proxy_address = self.chosen_proxy
+
         proxy_user_pass = self.proxies[proxy_address]
 
-        request.meta['proxy'] = proxy_address
         if proxy_user_pass:
-            basic_auth = 'Basic ' + base64.encodestring(proxy_user_pass)
+            request.meta['proxy'] = proxy_address
+            basic_auth = 'Basic ' + base64.b64encode(proxy_user_pass.encode()).decode()
             request.headers['Proxy-Authorization'] = basic_auth
+        else:
+            log.debug('Proxy user pass not found')
         log.debug('Using proxy <%s>, %d proxies left' % (
-                    proxy_address, len(self.proxies)))
+                proxy_address, len(self.proxies)))
 
     def process_exception(self, request, exception, spider):
         if 'proxy' not in request.meta:
             return
-
-        proxy = request.meta['proxy']
-        try:
-            del self.proxies[proxy]
-        except KeyError:
-            pass
-
-        log.info('Removing failed proxy <%s>, %d proxies left' % (
-                    proxy, len(self.proxies)))
+        if self.mode == Mode.RANDOMIZE_PROXY_EVERY_REQUESTS or self.mode == Mode.RANDOMIZE_PROXY_ONCE:
+            proxy = request.meta['proxy']
+            try:
+                del self.proxies[proxy]
+            except KeyError:
+                pass
+            request.meta["exception"] = True
+            if self.mode == Mode.RANDOMIZE_PROXY_ONCE:
+                self.chosen_proxy = random.choice(list(self.proxies.keys()))
+            log.info('Removing failed proxy <%s>, %d proxies left' % (
+                proxy, len(self.proxies)))
